a594ccac34f6fe8a383fff93d665fc04a390cb28
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-05-04 00:57:09 +0200

    wording

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,10 +6,11 @@ class Home extends Component {
     return (
       <article className="Home">
         <h1>Welcome to the Talao DAO!</h1>
-        <p>This is a <strong>prototype</strong> of the Talao DAO.</p>
-        <p>As a member of the Talao DAO, if you want to change some parameters of the DAO, then you can submit Objections. For now, you can try here an extremely simple Objection.</p>
-        <p>More sophisticated Objections and other stuff will come soon. For more more information about Talao, please head to the <a href="https://ico.talao.io" target="_blank" rel="noopener noreferrer">Talao ICO website</a>.</p>
-        <p><em>Happy testing and reading!</em></p>
+        <p>This is a <strong>prototype</strong> of the Talao DAO. Fasten your seat belt, some pretty good things are coming.</p>
+        <p>When? Right now. <strong>Just try this dApp.</strong></p>
+        <p>If you like it, then imagine the real thing after the ICO which will enable us to go full steam on dev ;)</p>
+        <p>Thanks for the visit!</p>
+        <p><em>Talao team</em></p>
       </article>
     );
   }
